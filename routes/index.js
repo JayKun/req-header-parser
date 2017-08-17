0519872aa48a0829c382da782a395556389efb9b
Jun <junkaiong11@gmail.com>
2017-08-17 03:36:00 +0000

    implemented api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/whoami', function(req, res){
+  var headers =  req.headers;
+  var language = headers['accept-language'];
+  var software = headers['user-agent'];
+  var ip = headers['x-forwarded-for'];
+  var data = {
+    ipaddress: ip,
+    language: language,
+    software: software 
+  };
+  res.json(data);
+});
+
 module.exports = router;
